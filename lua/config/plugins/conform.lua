1b03167684d08f44d857cf9589490c0befd61a92
Henry Bley <me@henrybley.com>
2025-06-14 13:43:06 +0200

    updates

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -2,24 +2,28 @@ return {
 	"stevearc/conform.nvim",
 	lazy = true,
 	event = { "BufReadPre", "BufNewFile" },
-	formatters_by_ft = {
-		lua = { "stylua" },
-		nix = { "nixfmt" },
-		typescript = { "prettier" },
-		typescriptreact = { "prettier" },
-		php = { "php-cs-fixer" },
-		python = { "isort", "black" },
-		rust = { "rustfmt" },
-	},
-	formatters = {
-		["php-cs-fixer"] = {
-			command = "php-cs-fixer",
-			args = {
-				"fix",
-				"$FILENAME",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				nix = { "nixfmt" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				php = { "php-cs-fixer" },
+				python = { "isort", "black" },
+				rust = { "rustfmt" },
 			},
-			stdin = false,
-		},
-	},
-	notify_on_error = true,
+			formatters = {
+				["php-cs-fixer"] = {
+					command = "php-cs-fixer",
+					args = {
+						"fix",
+						"$FILENAME",
+					},
+					stdin = false,
+				},
+			},
+			notify_on_error = true,
+		})
+	end,
 }
