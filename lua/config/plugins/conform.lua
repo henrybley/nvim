4fc33972af4ca124f413822291cbc310d2bcafcc
Henry Bley <me@henrybley.com>
2025-06-14 13:26:43 +0200

    updates

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -2,30 +2,24 @@ return {
 	"stevearc/conform.nvim",
 	lazy = true,
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				nix = { "nixfmt" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
-				php = { "php-cs-fixer" },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
-				rust = { "rustfmt", lsp_format = "fallback" },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "nixfmt" },
+		typescript = { "prettier" },
+		typescriptreact = { "prettier" },
+		php = { "php-cs-fixer" },
+		python = { "isort", "black" },
+		rust = { "rustfmt" },
+	},
+	formatters = {
+		["php-cs-fixer"] = {
+			command = "php-cs-fixer",
+			args = {
+				"fix",
+				"$FILENAME",
 			},
-			formatters = {
-				["php-cs-fixer"] = {
-					command = "php-cs-fixer",
-					args = {
-						"fix",
-						"$FILENAME",
-					},
-					stdin = false,
-				},
-			},
-			notify_on_error = true,
-		})
-	end,
+			stdin = false,
+		},
+	},
+	notify_on_error = true,
 }
